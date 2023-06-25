84d25f2bce34d3030762319c5a1f191288987471
Niiischal <nischal.khatiwada745@gmail.com>
2023-06-25 15:18:15 +0545

    completion of navbar

---
+++ styleit/src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+const Navbar = () => {
+  return (
+<>
+<nav className="navbar bg-body-tertiary fixed-top">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/" style={{fontFamily:"Ysabeau SC", color:'#006600'}}>StyleIt.</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="offcanvasNavbarLabel" style={{fontSize:'1.5rem', color:'#006600'}}>Navigation</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div className="offcanvas-body">
+        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3" style={{fontSize:'1.8rem', margin:'2.1rem 0 3rem 0', padding:'0 0 1.8rem 0'}}>
+          <li className="nav-item ">
+            <a className="nav-link active" aria-current="page" href="/home">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/product">Product</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/about">About Us</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/profile">Profile</a>
+          </li>
+        </ul>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi ex, in deserunt error unde accusamus omnis neque quasi deleniti beatae eos necessitatibus culpa delectus molestiae accusantium odit esse voluptas obcaecati?</p>
+      </div>
+    </div>
+  </div>
+</nav>
+</>
+  )
+}
+
+export default Navbar
