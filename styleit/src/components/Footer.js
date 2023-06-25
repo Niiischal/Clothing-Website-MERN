a08c9e8436b98b5b337a422a65d2f5808686257d
Niiischal <nischal.khatiwada745@gmail.com>
2023-06-25 22:33:31 +0545

    Navbar completed with footer in offcanvas menu but it is not responsive

---
+++ styleit/src/components/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+<>
+<footer className="bg-white dark:bg-gray-900">
+<div className="offcanvas-footer d-flex justify-content-evenly sticky-bottom bg-light">
+  <a href="/" class="footer-icon">
+    <i class="fab fa-facebook fa-lg" style={{ color: 'gray' }}></i>
+    <span class="sr-only">Facebook page</span>
+  </a>
+  <a href="/" class="footer-icon">
+    <i class="fab fa-twitter fa-lg" style={{ color: 'gray' }}></i>
+    <span class="sr-only">Twitter page</span>
+  </a>
+  <a href="/" class="footer-icon">
+    <i class="fab fa-instagram fa-lg" style={{ color: 'gray' }}></i>
+    <span class="sr-only">Instagram page</span>
+  </a>
+  <a href="/" class="footer-icon">
+    <i class="fab fa-discord"style={{ color: 'gray' }} ></i>
+    <span class="sr-only">Discord server</span>
+  </a>
+</div>
+</footer>
+
+</>
+  )
+}
+
+
+
+
+
+export default Footer
--- styleit/src/components/Navbar.js
+++ styleit/src/components/Navbar.js
@@ -1,38 +1,82 @@
 import React from 'react'
+import Footer from './Footer'
 const Navbar = () => {
   return (
 <>
 <nav className="navbar bg-body-tertiary fixed-top">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/" style={{fontFamily:"Ysabeau SC", color:'#006600'}}>StyleIt.</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+    <a className="navbar-brand" href="/" style={{ fontFamily: "Ysabeau SC" }}>
+      StyleIt<span style={{ color: "#006600", fontSize: "1rem" }}>.</span>
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="offcanvas"
+      data-bs-target="#offcanvasNavbar"
+      aria-controls="offcanvasNavbar"
+      aria-label="Toggle navigation"
+    >
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+    <div
+      className="offcanvas offcanvas-end"
+      tabIndex="-1"
+      id="offcanvasNavbar"
+      aria-labelledby="offcanvasNavbarLabel"
+    >
       <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasNavbarLabel" style={{fontSize:'1.5rem', color:'#006600'}}>Navigation</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <h5
+          className="offcanvas-title"
+          id="offcanvasNavbarLabel"
+          style={{ fontSize: "1.5rem", color: "#006600" }}
+        >
+          Navigation
+        </h5>
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
+        ></button>
       </div>
       <div className="offcanvas-body">
-        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3" style={{fontSize:'1.8rem', margin:'2.1rem 0 3rem 0', padding:'0 0 1.8rem 0'}}>
-          <li className="nav-item ">
-            <a className="nav-link active" aria-current="page" href="/home">Home</a>
+        <ul
+          className="navbar-nav justify-content-end flex-grow-1 pe-3"
+          style={{ fontSize: "1.8rem", margin: "2rem 0 2rem 0", padding: "0 0 1.8rem 0" }}
+        >
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/home">
+              Home
+            </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/product">Product</a>
+            <a className="nav-link active" aria-current="page" href="/product">
+              Product
+            </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/about">About Us</a>
+            <a className="nav-link active" aria-current="page" href="/about">
+              About Us
+            </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/profile">Profile</a>
+            <a className="nav-link active" aria-current="page" href="/profile">
+              Profile
+            </a>
           </li>
         </ul>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi ex, in deserunt error unde accusamus omnis neque quasi deleniti beatae eos necessitatibus culpa delectus molestiae accusantium odit esse voluptas obcaecati?</p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi ex, in deserunt
+          error unde accusamus omnis neque quasi deleniti beatae eos necessitatibus culpa
+          delectus molestiae accusantium odit esse voluptas obcaecati?
+        </p>
+        <Footer />
       </div>
     </div>
   </div>
 </nav>
+
+
 </>
   )
 }
