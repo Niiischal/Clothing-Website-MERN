4dab54bd0f62b817b7ade0ece394b8426b04f5a9
Niiischal <nischal.khatiwada745@gmail.com>
2023-06-29 21:49:42 +0545

    trying some images

--- styleit/src/components/Home.js
+++ styleit/src/components/Home.js
@@ -4,8 +4,8 @@ import image2 from '../images/image2.jpg'
 const Home = () => {
   return (
     <>
-        <div className='container' style={{display:'flex'}}>
-              <img src={image2} alt="" style={{zIndex:'1', width:'100%'}}/>
+        <div className='container' style={{display:'flex', justifyContent:"flex-start"}}>
+              <img src={image2} alt="" style={{zIndex:'1', width:'50%', height:'30em', marginTop:'30px'}}/>
     </div>
 
         </>
